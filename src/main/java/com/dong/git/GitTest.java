37659e7a8d364a7f0d3318e6762e92a4ab439711
xiaoyi <xiaoyi@62.com>
2023-12-02 16:33:32 +0800

    second commit

--- src/main/java/com/dong/git/GitTest.java
+++ src/main/java/com/dong/git/GitTest.java
@@ -8,5 +8,6 @@ package com.dong.git;
 public class GitTest {
     public static void main(String[] args) {
         System.out.println("Hello,Git!");
+        System.out.println("Hello,Git!!!");
     }
 }
