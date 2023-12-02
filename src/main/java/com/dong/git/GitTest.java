ef848c4f71b735e2818034c5a2ab31896199dec4
xiaoyi <xiaoyi@62.com>
2023-12-02 16:29:02 +0800

    first commit

---
+++ src/main/java/com/dong/git/GitTest.java
@@ -0,0 +1,12 @@
+package com.dong.git;
+
+/**
+ * @Author：whd
+ * @Description：
+ * @Date：2023/12/2 下午 04:23
+ */
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("Hello,Git!");
+    }
+}
