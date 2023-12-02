ce20e142665e2bb420f68216c749ee1ab457c18a
xiaoyi <xiaoyi@62.com>
2023-12-02 16:50:07 +0800

    master commit

--- src/main/java/com/dong/git/GitTest.java
+++ src/main/java/com/dong/git/GitTest.java
@@ -11,5 +11,6 @@ public class GitTest {
         System.out.println("Hello,Git!!!");
         System.out.println("Hello,Git!!!!!!");
         System.out.println("Hello,Git!!!!!!!!!");
+        System.out.println("master test");
     }
 }
